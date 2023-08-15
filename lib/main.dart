4aa86f48ddbefbb5c962d128c2df875f847adb61
kelle92 <gorankevic2104@gmail.com>
2023-08-15 22:33:20 +0200

    Removed unnecessary comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
-//Import packages
 import 'package:flutter/material.dart';
 import 'package:flutter_tasks/rounded_box_widget.dart';
 
-/* Potrebno da se nesto prikaze na ekranu
-void main() {
-  runApp();
-}
-*/
-
-//Basic APP
 void main() {
   runApp(
     const MaterialApp(
